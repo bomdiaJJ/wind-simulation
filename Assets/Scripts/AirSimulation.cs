d29e322edb87547c96e0ff0ba7c46bc928f94139
João Artur <joaoart42@gmail.com>
2023-06-08 03:03:56 -0300

    Add single direction step

--- Assets/Scripts/AirSimulation.cs
+++ Assets/Scripts/AirSimulation.cs
@@ -5,9 +5,24 @@ public class AirSimulation : MonoBehaviour {
     [SerializeField] private Vector2Int _gridSize;
     [SerializeField] private float _initialAmountOfAir;
 
-    public Node[,] Nodes { get; set; }
+    public Node[,] Nodes { get; set; } = new Node[0,0];
 
-    [Button("Generate Grid")]
+    [Button("Step Simulation")]
+    public void Step() {
+        for (int i = 0; i < _gridSize.x; i++) {
+            for (int j = 0; j < _gridSize.y; j++) {
+                if (i+1 >= Nodes.GetLength(0)) continue;
+
+                if (Nodes[i,j].AmountOfAir > Nodes[i+1,j].AmountOfAir) {
+                    float allAir = Nodes[i,j].AmountOfAir + Nodes[i+1,j].AmountOfAir;
+                    Nodes[i,j].AmountOfAir = allAir / 2f;
+                    Nodes[i+1,j].AmountOfAir = allAir / 2f;
+                }
+            }
+        }
+    }
+
+    [Button("Generate Node Grid")]
     public void GenerateGrid() {
         Nodes = new Node[_gridSize.x, _gridSize.y];
 
