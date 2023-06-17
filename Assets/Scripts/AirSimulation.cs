4885abcfd5fad9ea836bb142099fc30aedce4957
João Artur <joaoart42@gmail.com>
2023-06-17 06:18:14 -0300

    step changes densities to averages
    step changes densities from node and surroundings nodes to their average density (with x and y increasing from 0 to n)
    makes the air spread down and left first

--- Assets/Scripts/AirSimulation.cs
+++ Assets/Scripts/AirSimulation.cs
@@ -9,12 +9,48 @@ public class AirSimulation : MonoBehaviour {
 
     [Button("Step Simulation")]
     public void Step() {
+
         for (int i = 0; i < _gridSize.x; i++) {
             for (int j = 0; j < _gridSize.y; j++) {
-                // if (i+1 >= Nodes.GetLength(0)) continue;
-                // if (i-1 <= 0) continue;
-
+                float averageDensityBetweenNodeAndNeighbours = Nodes[i, j].Density;
+                int nodesContributing = 1;
                 
+                // node da esquerda
+                if (i > 0) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i-1, j].Density;
+                    nodesContributing++;
+                }
+                
+                // node da direita
+                if (i < _gridSize.x - 1) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i + 1, j].Density;
+                    nodesContributing++;
+                }
+
+                // node de cima
+                if (j > 0) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i, j - 1].Density;
+                    nodesContributing++;
+                }   
+                
+                // node de baixo
+                if (j < _gridSize.y - 1) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i, j+ 1].Density;
+                    nodesContributing++;
+                }
+
+                averageDensityBetweenNodeAndNeighbours /= nodesContributing;
+
+                Nodes[i, j].Density = averageDensityBetweenNodeAndNeighbours;
+
+                if (i > 0)
+                    Nodes[i - 1, j].Density = averageDensityBetweenNodeAndNeighbours;
+                if (i < _gridSize.x - 1)
+                    Nodes[i + 1, j].Density = averageDensityBetweenNodeAndNeighbours;
+                if (j > 0)
+                    Nodes[i, j - 1].Density = averageDensityBetweenNodeAndNeighbours;
+                if (j < _gridSize.y - 1)
+                    Nodes[i, j + 1].Density = averageDensityBetweenNodeAndNeighbours;
             }
         }
     }
