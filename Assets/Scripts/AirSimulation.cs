e9e27cf0ccc8ea7cbe1a85a942e750129276b406
João Artur <joaoart42@gmail.com>
2023-06-21 20:12:31 -0300

    makes step fully based on previous one
    intead of changing the simulation while itereting over it, iterates over the current state while storing the results in a new state that takes place after all iterations

--- Assets/Scripts/AirSimulation.cs
+++ Assets/Scripts/AirSimulation.cs
@@ -19,75 +19,54 @@ public class AirSimulation : MonoBehaviour {
 
     [Button("Step Simulation")]
     public void Step() {
+        Node[,] newNodes = Nodes.Clone() as Node[,];
 
-        for (int x = 0; x < _gridSize.x; x++) {
-            for (int y = 0; y < _gridSize.y; y++) {
-                // 10   11
-                //    v - Node velocity vector
-                // 00   01
-                int leftIndex = Nodes[x, y].NodePosition.x + Mathf.FloorToInt(Nodes[x, y].NodeVelocity.x);
-                int rightIndex = leftIndex + 1;
-                int bottomIndex = Nodes[x, y].NodePosition.y + Mathf.FloorToInt(Nodes[x, y].NodeVelocity.y);
-                int topIndex = bottomIndex + 1;
+        for (int i = 0; i < _gridSize.x; i++) {
+            for (int j = 0; j < _gridSize.y; j++) {
 
-                Node? nodeBottomLeft = 
-                    leftIndex < _gridSize.x && bottomIndex < _gridSize.y ? Nodes[leftIndex, bottomIndex] : null;
-                Node? nodeBottomRight = 
-                    rightIndex < _gridSize.x && bottomIndex < _gridSize.y ? Nodes[rightIndex, bottomIndex] : null;
-                Node? nodeTopLeft = 
-                    leftIndex < _gridSize.x && topIndex < _gridSize.y ? Nodes[leftIndex, topIndex] : null;
-                Node? nodeTopRight = 
-                    rightIndex < _gridSize.x && topIndex < _gridSize.y ? Nodes[rightIndex, topIndex] : null;
-
+                float averageDensityBetweenNodeAndNeighbours = Nodes[i, j].Density;
+                int nodesContributing = 1;
                 
+                // node da esquerda
+                if (i > 0) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i-1, j].Density;
+                    nodesContributing++;
+                }
+                
+                // node da direita
+                if (i < _gridSize.x - 1) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i + 1, j].Density;
+                    nodesContributing++;
+                }
+
+                // node de cima
+                if (j > 0) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i, j - 1].Density;
+                    nodesContributing++;
+                }   
+                
+                // node de baixo
+                if (j < _gridSize.y - 1) {
+                    averageDensityBetweenNodeAndNeighbours += Nodes[i, j+ 1].Density;
+                    nodesContributing++;
+                }
+
+                averageDensityBetweenNodeAndNeighbours /= nodesContributing;
+
+                newNodes[i, j].Density = averageDensityBetweenNodeAndNeighbours;
+
+                if (i > 0)
+                    newNodes[i - 1, j].Density = averageDensityBetweenNodeAndNeighbours;
+                if (i < _gridSize.x - 1)
+                    newNodes[i + 1, j].Density = averageDensityBetweenNodeAndNeighbours;
+                if (j > 0)
+                    newNodes[i, j - 1].Density = averageDensityBetweenNodeAndNeighbours;
+                if (j < _gridSize.y - 1)
+                    newNodes[i, j + 1].Density = averageDensityBetweenNodeAndNeighbours;
             }
         }
 
-        #region Old
-        // for (int i = 0; i < _gridSize.x; i++) {
-        //     for (int j = 0; j < _gridSize.y; j++) {
-        //         float averageDensityBetweenNodeAndNeighbours = Nodes[i, j].Density;
-        //         int nodesContributing = 1;
-                
-        //         // node da esquerda
-        //         if (i > 0) {
-        //             averageDensityBetweenNodeAndNeighbours += Nodes[i-1, j].Density;
-        //             nodesContributing++;
-        //         }
-                
-        //         // node da direita
-        //         if (i < _gridSize.x - 1) {
-        //             averageDensityBetweenNodeAndNeighbours += Nodes[i + 1, j].Density;
-        //             nodesContributing++;
-        //         }
-
-        //         // node de cima
-        //         if (j > 0) {
-        //             averageDensityBetweenNodeAndNeighbours += Nodes[i, j - 1].Density;
-        //             nodesContributing++;
-        //         }   
-                
-        //         // node de baixo
-        //         if (j < _gridSize.y - 1) {
-        //             averageDensityBetweenNodeAndNeighbours += Nodes[i, j+ 1].Density;
-        //             nodesContributing++;
-        //         }
-
-        //         averageDensityBetweenNodeAndNeighbours /= nodesContributing;
-
-        //         Nodes[i, j].Density = averageDensityBetweenNodeAndNeighbours;
-
-        //         if (i > 0)
-        //             Nodes[i - 1, j].Density = averageDensityBetweenNodeAndNeighbours;
-        //         if (i < _gridSize.x - 1)
-        //             Nodes[i + 1, j].Density = averageDensityBetweenNodeAndNeighbours;
-        //         if (j > 0)
-        //             Nodes[i, j - 1].Density = averageDensityBetweenNodeAndNeighbours;
-        //         if (j < _gridSize.y - 1)
-        //             Nodes[i, j + 1].Density = averageDensityBetweenNodeAndNeighbours;
-        //     }
-        // }
-        #endregion
+        Nodes = newNodes.Clone() as Node[,];
     }
 
     [Button("Generate Node Grid")]
